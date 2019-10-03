1dc6fc9bc904dd65b2b7b43ed222f14966832bf6
Oleksii Filonenko <brightone@protonmail.com>
2019-10-03 11:45:17 +0300

    Replace gen_tcp with gen_udp

--- lib/graphitex/client.ex
+++ lib/graphitex/client.ex
@@ -15,28 +15,33 @@ defmodule Graphitex.Client do
   @doc """
   Add a node and the service it provides to the directory.
   """
-  @spec metric(number, [String.t]) :: nil
+  @spec metric(number, [String.t()]) :: :ok
   def metric(value, namespace) when is_list(namespace) do
     metric({value, Enum.join(namespace, ".")})
   end
-  @spec metric(number, binary) :: nil
+
+  @spec metric(number, binary | String.t()) :: :ok
   def metric(value, namespace) when is_binary(namespace) do
     metric({value, namespace})
   end
-  @spec metric(number, binary|String.t, Float.t) :: nil
+
+  @spec metric(number, binary | String.t(), Float.t()) :: :ok
   def metric(value, namespace, ts) when is_float(ts) do
     metric({value, namespace, Float.round(ts, 1)})
   end
-  @spec metric({number, binary|String.t, Float.t}) :: nil
+
+  @spec metric({number, binary | String.t()} | {number, binary | String.t(), Float.t()}) :: :ok
   def metric(measurement) do
     GenServer.cast(@name, {:metric, pack_msg(measurement)})
   end
 
-  @spec metric_batch([{number, binary|String.t, Float.t}]) :: nil
+  @spec metric_batch([{number, binary | String.t(), Float.t()}]) :: :ok
   def metric_batch(batch) do
-    bulk_mgs = batch
-    |> Enum.map(&pack_msg/1)
-    |> Enum.join("")
+    bulk_mgs =
+      batch
+      |> Enum.map(&pack_msg/1)
+      |> Enum.join("")
+
     GenServer.cast(@name, {:metric, bulk_mgs})
   end
 
@@ -44,15 +49,17 @@ defmodule Graphitex.Client do
   # Private API
   #
 
-  @spec pack_msg({number, String.t}) :: binary
+  @spec pack_msg({number, String.t()}) :: charlist
   defp pack_msg({val, ns} = _arg) do
     pack_msg(val, ns, :os.system_time(:seconds))
   end
-  @spec pack_msg({number, String.t, Float.t}) :: binary
+
+  @spec pack_msg({number, String.t(), Float.t()}) :: charlist
   defp pack_msg({val, ns, ts} = _arg) do
     pack_msg(val, ns, ts)
   end
-  @spec pack_msg(number, String.t, Float.t) :: binary
+
+  @spec pack_msg(number, String.t(), Float.t()) :: charlist
   defp pack_msg(val, ns, ts) do
     '#{ns} #{val} #{ts}\n'
   end
@@ -68,27 +75,28 @@ defmodule Graphitex.Client do
   def connect(state) do
     port = Application.get_env(:graphitex, :port, 2003)
     host = Application.get_env(:graphitex, :host)
-    opts = [:binary, active: false]
-    Logger.info fn -> "Connecting to carbon at #{host}:#{port}" end
-    {:ok, socket} = :gen_tcp.connect(host, port, opts)
-    Logger.info "Connected"
-    %{state | socket: socket}
+    opts = [:binary, ip: host]
+    Logger.info(fn -> "Connecting to carbon at #{host}:#{port}" end)
+    {:ok, socket} = :gen_udp.open(port, opts)
+    Logger.info("Connected")
+    %{state | socket: socket, destination: {host, port}}
   end
 
   def terminate({:error, :closed}, state) do
     state
   end
+
   def terminate(_reason, %{socket: socket} = state) when not is_nil(socket) do
-    :gen_tcp.close(state.socket)
+    :gen_udp.close(state.socket)
   end
 
   def handle_cast(msg, %{socket: nil} = state) do
     connected_state = connect(state)
     handle_cast(msg, connected_state)
   end
-  def handle_cast({:metric, msg}, %{socket: socket} = state) do
-    :ok = :gen_tcp.send(socket, msg)
+
+  def handle_cast({:metric, msg}, state) do
+    :ok = :gen_udp.send(state.socket, state.destination, msg)
     {:noreply, state}
   end
-
 end
